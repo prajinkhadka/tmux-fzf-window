13518795d6bc8d1724a218d275a64ea82407af69
prajinkhadka <34759925+prajinkhadka@users.noreply.github.com>
2025-03-06 22:36:30 -0600

    Update fzf_window.sh

--- scripts/fzf_window.sh
+++ scripts/fzf_window.sh
@@ -1,18 +1,7 @@
-#!/usr/bin/env bash
+index=$(tmux list-windows -F '#I: #W' | \
+  fzf --height 40% --reverse --color hl:underline,hl+:reverse | \
+  cut -d: -f1)
 
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Main function
-tmux_fzf_window() {
-  local index
-  index=$(tmux list-windows -F '#I: #W' | \
-    fzf --height 40% --reverse --color hl:underline,hl+:reverse | \
-    cut -d: -f1)
-
-  if [ -n "$index" ]; then
-    tmux select-window -t "$index"
-  fi
-}
-
-# Run the function
-tmux_fzf_window
+if [ -n "$index" ]; then
+  tmux select-window -t "$index"
+fi
